c44779459df148c7d308323ba77a25ed3e1943f0
Sébastien BEAU <sebastien.beau@akretion.com>
2020-04-30 10:47:35 +0200

    Modify sale service. STEP 1: init module

---
+++ custom_shopinvader/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Custom Shopinvader",
+    "summary": "Custom shopinvader for learning",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["shopinvader"],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
